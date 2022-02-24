bf50994f1fcf73a044898080c7da071bd4dcdef9
Neev Parikh <neev_parikh@brown.edu>
2022-02-24 18:48:54 -0500

    Added arg parse and merged code with stencil

--- src/bin/node.rs
+++ src/bin/node.rs
@@ -1,5 +1,17 @@
+use clap::Parser;
+use debug_print::{debug_eprintln as edebug, debug_println as debug};
+
 use ip::lnx_config::LnxConfig;
 
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+pub struct Args {
+  /// Filename of the lnx file
+  #[clap(required = true)]
+  lnx_filename: String,
+}
+
 fn main() {
-    dbg!(LnxConfig::new("data/example.lnx"));
+  let args = Args::parse();
+  dbg!(LnxConfig::new(&args.lnx_filename));
 }
