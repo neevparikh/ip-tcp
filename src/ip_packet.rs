73e06efb58d3efc93fecca270c1d67d2e9ecb1a9
Neev Parikh <neev_parikh@brown.edu>
2022-02-26 16:57:45 -0500

    [WIP] - started designing IP struct

--- src/ip_packet.rs
+++ src/ip_packet.rs
@@ -3,10 +3,15 @@ use anyhow::Result;
 use super::protocol::Protocol;
 
 /// TODO
-pub struct IpPacket {}
+pub struct IpPacket {
+  header: Vec<u8>,
+  data: Vec<u8>,
+}
 
 impl IpPacket {
-  pub fn get_protocol(&self) -> Result<Protocol> {
-    todo!()
+  pub fn new() -> IpPacket {
+    todo!();
   }
+
+  pub fn version() -> &u8 {}
 }
