63777fefffec8ea952ee8c9cdd448944408b9019
Neev Parikh <neev_parikh@brown.edu>
2022-03-03 16:31:14 -0500

    Using if with cfg! instead of the #[]

--- src/utils.rs
+++ src/utils.rs
@@ -1,16 +1,18 @@
 #[macro_export]
 macro_rules! debug {
   ($($arg:tt)*) => {
-    #[cfg(debug_assertions)]
-    print!("[{}:{}] ", file!(), line!());
-    println!($($arg)*)
+    if cfg!(debug_assertions) {
+      print!("[{}:{}] ", file!(), line!());
+      println!($($arg)*)
+    }
   };
 }
 #[macro_export]
 macro_rules! edebug {
   ($($arg:tt)*) => {
-    #[cfg(debug_assertions)]
-    eprint!("[{}:{}] ", file!(), line!());
-    eprintln!($($arg)*)
+    if cfg!(debug_assertions) {
+      eprint!("[{}:{}] ", file!(), line!());
+      eprintln!($($arg)*)
+    }
   };
 }
